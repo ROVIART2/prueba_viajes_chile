3d25b47726ee04776ead1f288e88f51ddfd435ce
rovira gajardo <francisca-05@hotmail.com>
2021-05-14 16:24:12 -0400

    creando la carpeta

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip() // tooltip frases boton 
+
+
+  $("#iconTres").click(function(){
+    $("#parrafoTres").toggle();  // toggle ruta
+  });
+})
+
+
+
+  $("#iconDos").click(function(){
+    $("#parrafoDos").toggle(); // toggle montaña
+ });
+
+
+
+
+  $("#iconUno").click(function(){
+    $("#parrafoUno").toggle(); // toggle avion 
+ });
